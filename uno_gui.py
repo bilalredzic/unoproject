c67a6b62aaee69cca475584275938340460c3b31
Bilal Redzic <bilalredzic111@gmail.com>
2025-03-08 17:40:03 -0500

    win screen, reorganized gui a little

--- uno_gui.py
+++ uno_gui.py
@@ -16,40 +16,42 @@ class UnoGUI:
         self._screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pg.display.set_caption("UNO")
 
-        self.game = UnoGame([Player("Player 1"), Player("Player 2")])
-        self.game.create_deck()
-        self.game.start_game()
-
         self.ui_manager = gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT))
 
         self._create_ui_elements()
-
-        self.selected_card = None
-        self.card_images = {}
-        self.load_card_images()
+        self.start_new_game()
 
     def _create_ui_elements(self):
-        # Creates the draw button
         self.draw_button = gui.elements.UIButton(
             relative_rect=pg.Rect((635, 400), (120, 40)),
             text='Draw',
             manager=self.ui_manager
         )
 
-        # self.color_dropdown = gui.elements.UIDropDownMenu(
-        #    options_list=['Red', 'Blue', 'Green', 'Yellow'],
-        #    starting_option='Red',
-        #    relative_rect=pg.Rect((650, 400), (120, 40)),
-        #    manager=self.ui_manager
-        # )
-        # self.color_dropdown.hide()
-
-        # Creates the message box
         self.message_display = gui.elements.UITextBox(
             html_text="Welcome to Uno!<br>",
             relative_rect=pg.Rect((600, 10), (190, 380)),
             manager=self.ui_manager
         )
+
+        self.play_again_button = gui.elements.UIButton(
+            relative_rect=pg.Rect((300, 350), (200, 50)),
+            text="Play Again",
+            manager=self.ui_manager
+        )
+        self.play_again_button.hide()
+
+    def start_new_game(self):
+        self.game = UnoGame([Player("Player 1"), Player("Player 2")])
+        self.game.create_deck()
+        self.game.start_game()
+        self.game_over = False
+        self.winner = None
+        self.play_again_button.hide()
+        self.message_display.set_text("Welcome to Uno!")
+
+        self.card_images = {}
+        self.load_card_images()
 
     def load_card_images(self):
         color_options = ['red', 'blue', 'green', 'yellow', 'wild']
@@ -63,7 +65,6 @@ class UnoGUI:
                 scaled_image = pg.transform.scale(image, (CARD_WIDTH, CARD_HEIGHT))
                 self.card_images[(color, number)] = scaled_image
 
-            # Load action and wild card images.
             for action in action_values:
                 image_path = f"./images/{color}_{action}.png"
                 image = pg.image.load(image_path)
@@ -76,19 +77,26 @@ class UnoGUI:
 
         while running:
             time_delta = clock.tick(30) / 1000.0
-
-            # Handle events
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
 
                 self._handle_event(event)
 
-            self._update_display(time_delta)
+            if self.game_over:
+                self._show_win_screen()
+            else:
+                self._update_display(time_delta)
 
         pg.quit()
 
     def _handle_event(self, event):
+        if self.game_over:
+            if event.type == gui.UI_BUTTON_PRESSED and event.ui_element == self.play_again_button:
+                self.start_new_game()
+            self.ui_manager.process_events(event)
+            return
+
         if event.type == pg.MOUSEBUTTONDOWN:
             mouse_position = pg.mouse.get_pos()
             self._handle_mouse_click(mouse_position)
@@ -97,16 +105,12 @@ class UnoGUI:
             if event.ui_element == self.draw_button:
                 self._handle_draw_action()
 
-        # if event.type == gui.UI_DROP_DOWN_MENU_CHANGED:
-            # if event.ui_element == self.color_dropdown:
-                # selected_color = event.text.lower()
-                # self.game.current_color = Color(selected_color)
-                # self.color_dropdown.hide()
-                # self.game.next_turn()
-
         self.ui_manager.process_events(event)
 
     def _handle_mouse_click(self, position):
+        if self.game_over:
+            return
+
         x, y = position
         current_player = self.game.players[self.game.current_player_index]
 
@@ -116,17 +120,16 @@ class UnoGUI:
             if card_index < len(current_player.hand):
                 selected_card = current_player.hand[card_index]
                 if self.game.play_card(current_player, selected_card) is not False:
-                    if selected_card.value == "wild":
-                        self._update_game_display(f"{current_player.name} played a {selected_card.value}")
-                    else:
-                        self._update_game_display(f"{current_player.name} played a {selected_card.color.value} {selected_card.value}")
+                    self._update_game_display(f"{current_player.name} played {selected_card.color.value} {selected_card.value}")
                 else:
                     self._update_game_display(f"{current_player.name} played an invalid move")
 
-                # if selected_card.type == "WILD":
-                    # self.color_dropdown.show()
+        self._check_for_win()
 
     def _handle_draw_action(self):
+        if self.game_over:
+            return
+
         current_player = self.game.players[self.game.current_player_index]
 
         if len(self.game.deck) > 0:
@@ -134,7 +137,28 @@ class UnoGUI:
             current_player.hand.append(drawn_card)
             self._update_game_display(f"{current_player.name} drew a card")
             self.game.next_turn()
-            self._update_display(0)
+
+        self._check_for_win()
+
+    def _check_for_win(self):
+        for player in self.game.players:
+            if not player.hand:
+                self.game_over = True
+                self.winner = player.name
+
+    def _show_win_screen(self):
+        """Displays a separate win screen."""
+        self._screen.fill((0, 0, 0))
+
+        font = pg.font.Font(None, 64)
+        text_surface = font.render(f"{self.winner} WINS!", True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(SCREEN_WIDTH // 2, 250))
+        self._screen.blit(text_surface, text_rect)
+
+        self.play_again_button.show()
+        self.ui_manager.update(0)
+        self.ui_manager.draw_ui(self._screen)
+        pg.display.update()
 
     def _update_game_display(self, message):
         current_text = self.message_display.html_text
@@ -161,19 +185,9 @@ class UnoGUI:
         pg.display.update()
 
     def _draw_card(self, card, position):
-        if card.color != Color.WILD:
-            color_name = card.color.value
-        else:
-            color_name = 'wild'
-        if isinstance(card.value, int):
-            card_value = str(card.value)
-        else:
-            card_value = card.value
-        key = (color_name, card_value)
-        card_image = self.card_images.get(key)
-
-        if card_image is not None:
-            self._screen.blit(card_image, position)
+        key = (card.color.value if card.color != Color.WILD else 'wild', str(card.value))
+        if key in self.card_images:
+            self._screen.blit(self.card_images[key], position)
 
 
 def main():
