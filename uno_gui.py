46404e38b574b5e7e2ab16230d6bd372ba5aacd8
Evan9y4 <bowerev@mail.gvsu.edu>
2025-03-13 14:01:01 -0400

    Made Draw Pile

--- uno_gui.py
+++ uno_gui.py
@@ -22,13 +22,13 @@ class UnoGUI:
         self.start_new_game()
 
     def _create_ui_elements(self):
-        draw_card_image_path = "./images/blue_0.png"  # Change this to the actual image path
+        draw_card_image_path = "./images/uno_back.png"  # Change this to the actual image path
         draw_card_surface = pg.image.load(draw_card_image_path)
         draw_card_surface = pg.transform.scale(draw_card_surface, (CARD_WIDTH, CARD_HEIGHT))  # Scale it
 
         # Create an image button
         self.draw_button = gui.elements.UIImage(
-            relative_rect=pg.Rect((635, 400), (CARD_WIDTH, CARD_HEIGHT)),  # Position and size
+            relative_rect=pg.Rect((180, 200), (CARD_WIDTH, CARD_HEIGHT)),  # Position and size
             image_surface=draw_card_surface,
             manager=self.ui_manager
         )
@@ -54,7 +54,6 @@ class UnoGUI:
         self.winner = None
         self.play_again_button.hide()
         self.message_display.set_text("Welcome to Uno!")
-
         self.card_images = {}
         self.load_card_images()
 
@@ -179,7 +178,7 @@ class UnoGUI:
 
         if len(self.game.deck) > 0:
             top_card = self.game.discard_pile[-1]
-            self._draw_card(top_card, (350, 250))
+            self._draw_card(top_card, (350, 200))
 
         current_player = self.game.players[self.game.current_player_index]
 
